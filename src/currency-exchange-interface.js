b34e57604360af074160a77faabe4fdafe93453a
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-07-03 15:49:44 -0700

    Add .click function to main.js.

--- src/currency-exchange-interface.js
+++ src/currency-exchange-interface.js
@@ -1,13 +1,1 @@
-import $ from "jquery";
 
-
-export async function getExchangeRate() {
-  let response = await fetch(`https://v6.exchangerate-api.com/v6/eb383d1fe61093025c7a32ee/latest/USD`);
-  let rate;
-  if(response.ok && response.status === 200) {
-    rate = await response.json();
-  } else {
-    rate = false;
-  }
-  return rate;
-}
--- src/main.js
+++ src/main.js
@@ -2,13 +2,27 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
-import { getExchangeRate } from './currency-exchange-interface'
+// import { getExchangeRate } from './currency-exchange-interface';
+
+
+
 
 $(document).ready(function() {
   $("#exchange").click(function() {
 
-    getExchangeRate();
-    console.log(getExchangeRate);
+    let response = await fetch(`https://v6.exchangerate-api.com/v6/API_KEY/latest/USD`)
+    .then(function(response) {
+      if(!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    })
+    .catch(function(error) {
+      return error;
+    })
+    .then(function(returnedMessage) {
+      processRequest(returnedMessage);
+    });
 
   });
 });
