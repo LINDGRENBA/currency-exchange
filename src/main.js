97756a2e89783a3b129bc5691e3f0e4fd40f44c4
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-07-03 15:08:04 -0700

    Update business logic file names to follow best practice naming conventions, add initial API call to business logic and basic UI to main.js.

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { getExchangeRate } from './currency-exchange-interface'
 
 $(document).ready(function() {
   $("#exchange").click(function() {
 
+    getExchangeRate();
+    console.log(getExchangeRate);
 
-    
   });
 });
