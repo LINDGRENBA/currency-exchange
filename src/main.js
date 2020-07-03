e9235967e652ac96237c999ecd55bb68656ce7ac
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-07-03 14:54:54 -0700

    Update after running npm install, update test file name.

--- src/main.js
+++ src/main.js
@@ -2,3 +2,11 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+
+$(document).ready(function() {
+  $("#exchange").click(function() {
+
+
+    
+  });
+});
