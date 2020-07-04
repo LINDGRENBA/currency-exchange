c6c8749c9e69f94eaf95d86c8ff17c2503c70f05
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-07-04 12:37:21 -0700

    Update to complete seperation of logic, begin addressing syncronous code not waiting for asynchronous response.

--- src/currency-exchange-service.js
+++ src/currency-exchange-service.js
@@ -1,28 +1,17 @@
 export class makeExchange {
-  async getCurrencyExchangeRate(code) {
+  async getCurrencyExchangeRate() {
     try {
       let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       let jsonifiedResponse;
       if(response.ok && response.status === 200) {
-        const setCode = await response.json()
-        jsonifiedResponse = setCode + "." + conversion_rates + "." + code;
+        jsonifiedResponse = await response.json();
       } else {
-        jsonifiedResponse = error;
+        jsonifiedResponse = false;
       }
       return jsonifiedResponse;
     } catch(error) {
-      return error;
+      return false;
     }
   }
 }
 
-
-// export async function callAPI() {
-//   const responseFromAPI = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
-//   if(!responseFromAPI.ok) {
-//     return showError(responseFromAPI.statusText); //write showError function
-//   } else {
-//     const responseJsonified = await responseFromAPI.json();
-//     return provideExchangeRate(responseJsonified); //write provideExchangeRate function
-//   }
-// }
--- src/main.js
+++ src/main.js
@@ -10,29 +10,32 @@ $(document).ready(function() {
 
     const usDollarInput = $("#us-dollars").val();
     const code = $("#currency-code").val();
-    // DELETE BELOW
-    console.log('initial code: ' + code);
+    //DELETE BELOW
+    console.log('code from user: ' + code);
 
     (async () => {
       let exchangeRate = new makeExchange();
-      const response = await exchangeRate.getCurrencyExchangeRate(code);  //do we need to .getCurrencyRateExchange(param)? with something passed?
+      const response = await exchangeRate.getCurrencyExchangeRate();
       if(!response) {
         $("#error").html(`<h1>There has been an error processing your request</h1>`);
       } else {
         getRate(response);
         //DELETE BELOW
-        console.log('async response: ' + response);
+        console.log('async response: ' + response);  //THIS IS RETURNING AN OBJECT, BUT TOO LATE
       }
     })();
 
     // let currencyCodes = ['EUR', 'TRY', 'KRW', 'RUB', 'MXN', 'JPY']; //do loop, if convertToInput = currencyCodes[i], then add currencyCodes[i] to result.conversion_rates.{i goes here} to get the specific value, then multiply it by the user's input for us dollars
 
-    // callAPI();
-
     function getRate(response) {
       if(response) {
+        let returnRate = response.conversion_rates + "." + code;
+        //DELETE BELOW
+        console.log('in getRate first: ' + returnRate);  //THIS IS HAPPENING BEFORE ASYNC RETURNS
+        let totalCurrency = usDollarInput * returnRate;
+        //DELETE BELOW
+        console.log('in getRate second: ' + totalCurrency);  //THIS IS HAPPENING BEFORE ASYNC RETURNS
         $("#usd").text(usDollarInput);
-        let totalCurrency = usDollarInput * response;
         $("#other-currency").text(totalCurrency);
       } else {
         $("#error").html(`<h1>There has been an error processing your request</h1>`);
